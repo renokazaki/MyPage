6cef8c8c14834498d9c4e4c3b51f2d2448636f70
renokazaki <ren84690@gmail.com>
2024-10-07 22:51:08 +0900

    簡単なスマホサイズのレイアウト作成

---
+++ src/app/Components/Contents/Contents.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Contents = () => {
+  return (
+    <div className="flex flex-col overflow-y-scroll">
+      <div>ポートフォリオ</div>
+      <div className="flex justify-center items-center h-40">
+        ポートフォリオ
+      </div>
+      <div className="flex justify-center items-center h-40">
+        ポートフォリオ
+      </div>
+      <div className="flex justify-center items-center h-40">
+        ポートフォリオ
+      </div>
+      <div className="flex justify-center items-center h-40">
+        ポートフォリオ
+      </div>
+
+      <div>スキルセット</div>
+      <div className="flex justify-center items-center h-40">Javascript</div>
+      <div className="flex justify-center items-center h-40">React</div>
+      <div className="flex justify-center items-center h-40">NextJs</div>
+      <div className="flex justify-center items-center h-40">Servicenow</div>
+    </div>
+  );
+};
+
+export default Contents;
---
+++ src/app/Components/Navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="flex justify-around items-center bg-slate-500 text-white h-full">
+      <div>home</div>
+      <div>home</div>
+      <div>home</div>
+    </div>
+  );
+};
+
+export default Navigation;
