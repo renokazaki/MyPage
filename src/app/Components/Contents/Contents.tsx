461c61f39b93314496acca6ee73a0bd0c1526ac6
renokazaki <ren84690@gmail.com>
2024-10-08 23:44:54 +0900

    スマホ用のレイアウトの実装

--- src/app/Components/Contents/Contents.tsx
+++ src/app/Components/Contents/Contents.tsx
@@ -1,27 +1,100 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image"; // Imageコンポーネントをインポート
+
+interface Information {
+  id: string;
+  title: string;
+  description: string;
+  src: string;
+  link: string;
+}
+interface TechnologyStack {
+  id: string;
+  title: string;
+  description: string;
+  src: string;
+}
+
+interface Data {
+  portfolio: Information[];
+  technologyStack: TechnologyStack[];
+}
 
 const Contents = () => {
+  //jsonデータ格納用
+  const [datas, setDatas] = useState<Data | undefined>(undefined);
+
+  //情報取得用関数
+  const getData = async () => {
+    try {
+      const response = await fetch("/data/information.json");
+      if (!response.ok) {
+        throw new Error("ネットワーク応答が正常ではありません");
+      }
+      const jsonData = await response.json();
+      setDatas(jsonData);
+    } catch (error) {
+      console.error("フェッチエラー:", error);
+    }
+  };
+
+  // JSONデータをフェッチする処理
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="flex flex-col overflow-y-scroll">
-      <div>ポートフォリオ</div>
-      <div className="flex justify-center items-center h-40">
-        ポートフォリオ
+      {/* ポートフォリオ */}
+      <div className="border-b-2 border-dashed border-black w-1/4 mt-4">
+        My Portfolio
       </div>
-      <div className="flex justify-center items-center h-40">
-        ポートフォリオ
-      </div>
-      <div className="flex justify-center items-center h-40">
-        ポートフォリオ
-      </div>
-      <div className="flex justify-center items-center h-40">
-        ポートフォリオ
-      </div>
+      {datas ? (
+        datas.portfolio.map((data) => (
+          <div key={data.id} className="flex flex-col items-center ">
+            <div className="flex flex-col items-center mt-4">{data.title}</div>
+            <Image
+              src={data.src}
+              alt=""
+              width={150} // 幅を指定
+              height={150} // 高さを指定
+              className="w-40 h-40 my-4" // 画像を正方形に表示
+            />
+            <p className="border-b border-black w-1/2"></p>
+          </div>
+        ))
+      ) : (
+        <div>データを読み込んでいます...</div>
+      )}
+      <p className="border-2 border-dashed border-gray-300 w-full mt-8 mb-"></p>
 
-      <div>スキルセット</div>
-      <div className="flex justify-center items-center h-40">Javascript</div>
-      <div className="flex justify-center items-center h-40">React</div>
-      <div className="flex justify-center items-center h-40">NextJs</div>
-      <div className="flex justify-center items-center h-40">Servicenow</div>
+      {/* 技術スタック */}
+      <div className="mb-8">
+        <div className="border-b-2 border-dashed border-black w-1/4 mt-4">
+          技術スタック
+        </div>
+        {datas ? (
+          datas.technologyStack.map((tech) => (
+            <div key={tech.id} className="flex flex-col items-center">
+              <div className="flex flex-col items-center mt-4">
+                {tech.title}
+              </div>
+              <Image
+                src={tech.src}
+                alt=""
+                width={150} // 幅を指定
+                height={150} // 高さを指定
+                className="w-40 h-40 my-4" // 画像を正方形に表示
+              />
+              <p className="border-b border-black w-1/2"></p>
+            </div>
+          ))
+        ) : (
+          <div>データを読み込んでいます...</div> // ローディングメッセージ
+        )}
+      </div>
     </div>
   );
 };
--- src/app/Components/Navigation/Navigation.tsx
+++ src/app/Components/Navigation/Navigation.tsx
@@ -1,11 +1,67 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { FcPrevious } from "react-icons/fc";
 
 const Navigation = () => {
+  const [isTouch, setIsTouch] = useState(false);
+  const handleClick = () => {
+    setIsTouch(!isTouch);
+  };
+
+  const closeModal = (isTouch: boolean) => {
+    setIsTouch(!isTouch);
+  };
+
   return (
-    <div className="flex justify-around items-center bg-slate-500 text-white h-full">
-      <div>home</div>
-      <div>home</div>
-      <div>home</div>
+    <div
+      className="
+    fixed w-full h-16 flex  pl-4 justify-start items-center bg-slate-500 text-white"
+    >
+      {/* スマホサイズのハンバーガーアイコン */}
+
+      {isTouch ? (
+        <div>
+          <FcPrevious size={24} onClick={handleClick} />
+          {isTouch && (
+            <div
+              onClick={() => closeModal(isTouch)}
+              className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+            >
+              <div
+                onClick={(e) => e.stopPropagation()} // モーダル内部のクリックイベントをバブルアップさせない
+                className="bg-white p-8 rounded-lg w-3/4 h-2/4 flex flex-col items-center justify-around"
+              >
+                <h2 className="text-xl font-bold mb-4 text-co  text-slate-600">
+                  モーダルのタイトル
+                </h2>
+                <h2 className="text-xl font-bold mb-4 text-co  text-slate-600">
+                  モーダルのタイトル
+                </h2>
+                <h2 className="text-xl font-bold mb-4 text-co  text-slate-600">
+                  モーダルのタイトル
+                </h2>
+
+                <div
+                  onClick={() => closeModal(isTouch)}
+                  className="flex justify-center items-center bg-red-500 text-white px-4 py-2 w-40 bottom-0"
+                >
+                  閉じる
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <FaBars size={24} onClick={handleClick} />
+      )}
+      {/* デスクトップサイズのナビゲーションメニュー */}
+      <div className="fixed w-full h-16 hidden sm:flex flex-row justify-around items-center bg-slate-500 text-white">
+        <div>home</div>
+        <div>Portfolio</div>
+        <div>技術スタック</div>
+      </div>
     </div>
   );
 };
